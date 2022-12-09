2f5a98c29118faeaae693b683bb0e9c36098310f
Sasquach1187 <113910447+Sasquach1187@users.noreply.github.com>
2022-12-09 08:27:45 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,51 @@
 function lights () {
     Red()
+    red_light = true
     basic.pause(2000)
     Green()
+    green_light = true
     basic.pause(2000)
     yellow()
+    yellow_light = true
     basic.pause(2000)
 }
+function crosswalk () {
+    yellow()
+    basic.pause(2000)
+    Red()
+    basic.pause(2000)
+}
+input.onButtonPressed(Button.A, function () {
+    dobe = true
+    crosswalk()
+    basic.pause(1000)
+    countdown = 10
+    for (let index = 0; index < 10; index++) {
+        basic.showNumber(countdown)
+        countdown += -1
+        basic.pause(500)
+    }
+    basic.pause(500)
+    basic.showIcon(IconNames.No)
+    basic.pause(500)
+    dobe = false
+})
+input.onButtonPressed(Button.B, function () {
+    dobe = true
+    crosswalk()
+    basic.pause(1000)
+    countdown = 10
+    for (let index = 0; index < 10; index++) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        basic.showNumber(countdown)
+        countdown += -1
+        basic.pause(500)
+    }
+    basic.pause(500)
+    basic.showIcon(IconNames.No)
+    basic.pause(500)
+    dobe = false
+})
 function yellow () {
     range = strip.range(1, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Yellow))
@@ -30,30 +70,20 @@ function Red () {
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
-let distance = 0
 let range: neopixel.Strip = null
+let yellow_light = false
+let green_light = false
+let red_light = false
 let strip: neopixel.Strip = null
+let countdown = 0
 let dobe = false
+dobe = false
+countdown = 0
 strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 strip.setBrightness(20)
-basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
-    basic.showString("" + (distance))
-    basic.pause(2000)
-    if (distance >= 20) {
-        dobe = true
-    }
-})
+basic.showIcon(IconNames.No)
 basic.forever(function () {
     if (dobe == false) {
         lights()
-    }
-    if (dobe == true) {
-        control.reset()
     }
 })
