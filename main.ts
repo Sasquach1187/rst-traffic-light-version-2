44f8a6e375f544248394cdc6b90c05a25d07807e
Sasquach1187 <113910447+Sasquach1187@users.noreply.github.com>
2022-12-15 08:53:17 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,26 +5,27 @@ radio.onReceivedNumber(function (receivedNumber) {
 function lights () {
     Red()
     red_light = true
-    basic.pause(2000)
+    basic.pause(34000)
     Green()
     green_light = true
-    basic.pause(2000)
+    basic.pause(56000)
     yellow()
     yellow_light = true
-    basic.pause(2000)
+    basic.pause(4000)
 }
 function crosswalk () {
-    yellow()
-    basic.pause(10000)
     Red()
-    basic.pause(10000)
+    basic.pause(2000)
 }
 input.onButtonPressed(Button.A, function () {
     dobe = true
     crosswalk()
+    basic.showIcon(IconNames.Yes)
+    basic.pause(15000)
     countdown = 10
     for (let index = 0; index < 10; index++) {
         basic.showNumber(countdown)
+        basic.pause(1000)
         countdown += -1
     }
     basic.showIcon(IconNames.No)
@@ -34,10 +35,13 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     dobe = true
     crosswalk()
+    basic.showIcon(IconNames.Yes)
+    basic.pause(15000)
     countdown = 10
     for (let index = 0; index < 10; index++) {
         music.playTone(262, music.beat(BeatFraction.Whole))
         basic.showNumber(countdown)
+        basic.pause(1000)
         countdown += -1
     }
     basic.showIcon(IconNames.No)
@@ -51,26 +55,6 @@ function yellow () {
     range.showColor(neopixel.colors(NeoPixelColors.Black))
     range = strip.range(0, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
-}
-function sensor () {
-    distance = 6
-    countdown = counter
-    basic.showNumber(counter)
-    for (let index = 0; index < 4; index++) {
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        control.waitMicros(2)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        control.waitMicros(10)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
-        if (distance < 5) {
-            counter += 1
-            basic.showNumber(counter)
-        }
-        if (counter == 4) {
-            counter += 0
-        }
-    }
 }
 function Green () {
     range = strip.range(2, 1)
@@ -88,18 +72,17 @@ function Red () {
     range = strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
-let counter = 0
 let distance = 0
+let counter = 0
 let range: neopixel.Strip = null
+let countdown = 0
 let yellow_light = false
 let green_light = false
 let red_light = false
 let dobe = false
 let strip: neopixel.Strip = null
-let countdown = 0
 radio.setGroup(255)
 basic.showIcon(IconNames.No)
-countdown = 0
 strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 strip.setBrightness(58)
 basic.forever(function () {
@@ -108,5 +91,19 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    sensor()
+    counter = 0
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        control.waitMicros(2)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        control.waitMicros(10)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+        if (distance < 10) {
+            counter += 1
+        }
+    }
+    if (counter == 4) {
+        Green()
+    }
 })
